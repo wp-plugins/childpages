76f222da854cfaf9a625003f642331fe7d7ef40f
janhenckens <janhenckens@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-29 17:34:58 +0000

    tagging 1.0
    git-svn-id: https://plugins.svn.wordpress.org/childpages/tags/1.0@1170346 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,38 @@
+=== Plugin Name ===
+Contributors: janhenckens
+Donate link: http://onedge.be/donate
+Tags: pages, widget, sub pages, child pages
+Requires at least: 3.0.1
+Tested up to: 4.2.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Displays the childpages of the page you're currently on
+
+== Description ==
+
+The child pages widget automatically displays the childpages of the page you're currently viewing. If the current page has no child pages, the widget will not be visible.
+You can also add css class to both the `<ul>` and the `<li>` tags, to match the widget's styling with your theme.
+
+
+= Features to be a added =
+- Depth paramter to specify how many levels of childpages you want to show
+- "Start from" option: to show a specific set of childpages on every pages
+
+== Installation ==
+
+1. Upload the `wp-childpages` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add the widget to a widget area or sidebar
+
+
+== Screenshots ==
+
+1. After you activate the plugin, a new widget will be available in the Appearance settings. Here you can add a title to the widget and css classes to the entire widget and the individual items.
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
---
+++ includes/class-wp-childpages-widget.php
@@ -0,0 +1,111 @@
+<?php
+    /**
+    * Example Widget Class
+    */
+    class Subpages_Widget extends WP_Widget {
+
+        /**
+         * Register widget with WordPress.
+         */
+        function __construct() {
+            parent::__construct(
+                'subpages_widget', // Base ID
+                __( 'Child pages', 'wp-childpages' ), // Name
+                array( 'description' => __( 'Lists the child pages of the current page', 'wp-childpages' ), ) // Args
+            );
+        }
+
+
+        /**
+         * Display the widget
+         * @param $args
+         * @param $instance
+         */
+        function widget($args, $instance)
+        {
+            extract($args);
+
+            $postid = get_the_ID();
+            $args = array(
+                'parent' => $postid,
+	            'hierarchical' => 1,
+                'post_type'   => 'page',
+                'post_status' => 'publish'
+            );
+            $subpages = get_pages( $args );
+
+
+            if( count( $subpages ) != 0 ) {
+
+
+                // The widget title
+                $title = apply_filters('widget_title', $instance['title']);
+                $before_title = "<h2 class='widget-title'>";
+                $after_title = "</h2>";
+                ?>
+                <aside id="wp-childpages" class="widget wp-childpages widget_pages">
+                    <?php if ( $title ) echo $before_title . $title . $after_title; ?>
+                    <ul class="<?php echo (isset($instance['menu-class'])) ? $instance['menu-class'] : '' ?>">
+	                <?php foreach($subpages as $subpage) { ?>
+	                    <li class="<?php echo (isset($instance['item-class'])) ? $instance['item-class'] : 'page_item' ?>"><a href="<?php echo get_permalink($subpage->ID); ?>">
+			                    <?php echo $subpage->post_title; ?>
+	                        </a>
+	                    </li>
+	                <?php } ?>
+                    </ul>
+
+                </aside>
+            <?php
+            }
+
+        }
+
+        /**
+         * Update the widget
+         * @param $new_instance
+         * @param $old_instance
+         * @return mixed
+         */
+        function update( $new_instance, $old_instance ) {
+            $instance = $old_instance;
+            $instance['title'] = strip_tags( $new_instance['title'] );
+            $instance['menu-class'] = strip_tags( $new_instance['menu-class'] );
+            $instance['item-class'] = strip_tags( $new_instance['item-class'] );
+            // $instance['depth'] = strip_tags( $new_instance['depth'] );
+            return $instance;
+        }
+
+        /**
+         * Render the widget form in the Dashboard
+         * @param $instance
+         */
+        function form($instance) {
+            ?>
+            <p>
+                <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title', 'wp-childpages'); ?></label>
+                <input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+            </p>
+            <p>
+                <label for="<?php echo $this->get_field_id( 'menu-class' ); ?>"><?php _e('Menu class', 'wp-childpages'); ?></label>
+                <input id="<?php echo $this->get_field_id( 'menu-class' ); ?>" name="<?php echo $this->get_field_name( 'menu-class' ); ?>" value="<?php echo $instance['menu-class']; ?>" style="width:50%;" />
+            </p>
+            <p>
+                <label for="<?php echo $this->get_field_id( 'item-class' ); ?>"><?php _e('Item class', 'wp-childpages'); ?></label>
+                <input id="<?php echo $this->get_field_id( 'item-class' ); ?>" name="<?php echo $this->get_field_name( 'item-class' ); ?>" value="<?php echo $instance['item-class']; ?>" style="width:50%;" />
+            </p>
+<!-- 	        <p>
+                <label for="<?php echo $this->get_field_id( 'depth' ); ?>"><?php _e('Depth', 'wp-childpages'); ?></label>
+                <input id="<?php echo $this->get_field_id( 'depth' ); ?>" name="<?php echo $this->get_field_name( 'depth' ); ?>" value="<?php echo $instance['depth']; ?>" style="width:100%;" type="number" max="10" min="-1" />
+		        <em><?php _e('Set to -1 to show all subpages', 'wp-childpages'); ?></em>
+            </p> -->
+        <?php }
+
+    }
+
+    /**
+     * Register the widget
+     */
+    function register_subpages_widget() {
+        register_widget( 'Subpages_Widget' );
+    }
+    add_action( 'widgets_init', 'register_subpages_widget' );
